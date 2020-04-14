7523f63bea524e219589397166a468747818985f
Eloy Chacon <eloychacon@miguayaba.com>
2020-04-14 10:37:15 -0500

    creando server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
+import { IResolvers, makeExecutableSchema} from 'graphql-tools';
+import { GraphQLSchema } from 'graphql';
+import graphQLHTTP from 'express-graphql';
 
 const app = express();
 
@@ -8,12 +11,40 @@ app.use('*', cors());
 
 app.use(compression());
 
-app.use('/', (re,res)=>{
-    res.send('Bienvenido al curso de GraphQL');
-});
+const typeDefs = `
+    type Query {
+        hola: String!
+        holaConNombre(nombre: String!): String!
+        holaAlCurso: String!
+    }
+`;
+
+const resolvers: IResolvers = {
+    Query : {
+        hola(): String{
+            return "Hola Mundo";
+        },
+        holaConNombre(__: void, {nombre}): string {
+            return 'Hola Mundo ${nombre}';
+        },
+        holaAlCurso(): String{
+            return "Hola Mundo al curso de GraphQL";
+        }
+    }
+} 
+
+const schema: GraphQLSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+app.use('/', graphQLHTTP({
+    schema,
+    graphiql: true
+
+}));
 
 const PORT = 5300;
 app.listen(
     {port : PORT},
-    ()=> console.log(`Hola GraphQL http://localhost:${PORT}`)
+    ()=> console.log(`Hola GraphQL http://localhost:${PORT}/graphql`)
 )
