2e1d69866f9a68aaba319c7efd2b5e1f19297344
Eloy Chacon <eloychacon@miguayaba.com>
2020-04-13 22:32:09 -0500

    Curso GraphQL

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+
+app.use('*', cors());
+
+app.use(compression());
+
+app.use('/', (re,res)=>{
+    res.send('Bienvenido al curso de GraphQL');
+});
+
+const PORT = 5300;
+app.listen(
+    {port : PORT},
+    ()=> console.log(`Hola GraphQL http://localhost:${PORT}`)
+)
